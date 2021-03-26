495d643f308603f3cd0afe0986571217d6fad343
pedro <pfraguas@udesa.edu.ar>
2021-03-26 18:12:41 -0300

    entrega rick y morty

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import Card from './components/Card'
+import data from './rickandmorty.json'
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h2>Rick & Morty Characters</h2>
+      </header>
+      <main>
+        <section class="section" style={{'background-color': '#FBF8F3;'}}>
+          <div class="hero">
+            <div class="hero-body">
+              <div class="content">
+                
+                {/*<!-- Contenedor de tarjetas -->*/}
+                <div class="grid grid-cols-3 grid-gap-3">
+
+                  {
+                    data.map(function(char, idx) {
+                        return < Card info={char} key={idx}/>
+                    })
+                  }
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+      <footer>
+        <p>
+          Pedro Fraguas
+        </p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Card.js
@@ -0,0 +1,19 @@
+function Card(props) {
+    return(
+        <div class="card u-flex u-flex-column">
+            <div class="card__container">
+                <div class="card__image" style={{"background-image": "url(" + props.info.image + ")"}}></div>
+            </div>
+        
+            <div class="content">
+               <p class="title">{props.info.name}</p>
+               <p>Status: {props.info.status}</p>
+               <p>Species: {props.info.species}</p>
+               <p>Origin: {props.info.origin.name}</p>
+            </div>
+                  
+        </div>
+    )
+}
+
+export default Card
